a699bfd3e98b9e3b292041f46a31d498550c6e8b
renderr3 <rendergithub3@gmail.com>
2025-09-24 16:02:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ from os import getenv
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
+OWNER_ID = int(getenv("OWNER_ID", "8075836012"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1003082043736"))
+FORCESUB = getenv("FORCESUB", "-1003025510345")
 DEFAULT_SESSION = getenv("DEFAULT_SESSION", "") # this is just to help if you dont want to force your bot user to login or if they not interested
+
